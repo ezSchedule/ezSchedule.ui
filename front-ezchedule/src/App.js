1012b6791f960f66245d810f3290a516d48e186a
VinyProgramador <vinicius.nunes@sptech.school>
2023-02-06 15:30:53 -0300

    Primeiro banner de propaganda codificado, seguindo o figma.

--- front-ezchedule/src/App.js
+++ front-ezchedule/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AdvertisingBanner from './components/AdvertisingBanner';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AdvertisingBanner />
     </div>
   );
 }
---
+++ front-ezchedule/src/components/AdvertisingBanner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './AdvertisingBanner.css'
+
+const AdvertisingBanner = () => {
+  return (
+    <div className='mainAdvertisingBanner'>
+        <div className='advertisingBanner'>
+        <div className='textAdvertising'>
+            <h1>
+                A um passo da <span>organização!</span>
+            </h1>
+            <p>Problemas para organizar sua festa? Podemoste ajudar com isso, apresentamos para você o Schedule!</p>
+            <button>
+                Cadastro
+            </button>
+        </div>
+        <div></div>
+    </div>
+    </div>
+  )
+}
+
+export default AdvertisingBanner

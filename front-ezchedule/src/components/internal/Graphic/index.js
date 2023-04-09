cb50d64f38627eed7115ca7982f2c30a6d169eea
VinyProgramador <vinicius.nunes@sptech.school>
2023-04-09 18:36:32 -0300

    Tela de grafico.

---
+++ front-ezchedule/src/components/internal/Graphic/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './graphic.css'
+
+const GraphicInside = () => {
+  return (
+    <>
+        <div className='mainGraphicInside'>
+            <div></div>
+        </div>
+    </>
+  )
+}
+
+export default GraphicInside
--- front-ezchedule/src/pages/Administrator/Graphic/index.js
+++ front-ezchedule/src/pages/Administrator/Graphic/index.js
@@ -2,13 +2,20 @@ import React from 'react'
 import './graphic.css'
 import Sidebar from '../../../components/internal/SideBar/index';
 import HeaderInternal from '../../../components/internal/Header';
+import GraphicInside from '../../../components/internal/Graphic';
 
 const Graphic = () => {
   return (
+    <>
     <div className='mainGraphic'>
         <Sidebar />
         <HeaderInternal text="Grafico"/>
     </div>
+    <div className='graphic'>
+      <div></div>
+      <span></span>
+    </div>
+    </>
   )
 }
 

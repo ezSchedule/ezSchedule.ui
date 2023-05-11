0296ceb3d0aa1de4fbda643259ba73f449ff4409
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-11 12:10:13 -0300

    feat

--- front-ezchedule/src/App.js
+++ front-ezchedule/src/App.js
@@ -1,49 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import RegisterPart1 from './pages/register/RegisterPart1';
-import RegisterPart2 from './pages/register/RegisterPart2';
-import RegisterPart3 from './pages/register/RegisterPart3';
-import NotFound from './pages/NotFound';
-import Calendar from './pages/Administrator/Calendar/index';
-import Graphic from './pages/Administrator/Graphic';
-import SendEmail from './pages/recoverPassword/SendEmail';
-import SecurityCode from './pages/recoverPassword/SecurityCode';
-import UpdatePassword from './pages/recoverPassword/UpdatePassword';
-import Service from './pages/Administrator/Service/Index';
-import Payment from './pages/Administrator/Payment';
-import Forum from './pages/Administrator/Forum';
-import Configuration from './pages/Administrator/Configuration';
-import PersonalSettings from './pages/Administrator/SettingsPerson';
-import CondominiumSettings from './pages/Administrator/SettingsCondominium';
-import NewService from './pages/Administrator/NewService';
+import RoutesSystem from '../src/routes';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/registerPart1' element={<RegisterPart1 />} />
-          <Route path='/registerPart2' element={<RegisterPart2 />} />
-          <Route path='/registerPart3' element={<RegisterPart3 />} />
-          <Route path='/homeAdm' element={<Calendar />} />
-          <Route path='/graphicAdm' element={<Graphic />} />
-          <Route path='/sendEmail' element={<SendEmail />} />
-          <Route path='/securityCode' element={<SecurityCode />} />
-          <Route path='/updatePassword' element={<UpdatePassword />} />
-          <Route path='/servicesAdm' element={<Service />} />
-          <Route path='/newService' element={<NewService />} />
-          <Route path='/paymentAdm' element={<Payment />} />
-          <Route path='/forumAdm' element={<Forum />} />
-          <Route path='/configurationAdm' element={<Configuration />} />
-          <Route path='/personSettingsAdm' element={<PersonalSettings />} />
-          <Route path='/condominiumSettings' element={<CondominiumSettings />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RoutesSystem />
     </div>
   );
 }
---
+++ front-ezchedule/src/components/internal/SidebarTenant/Index.js
@@ -0,0 +1,36 @@
+import './sidebarTenant.css'
+import imgCalendar from '../../assets/calendarInternal.png'
+import imgSuitcase from '../../assets/suitcase.png'
+import imgCalendarClock from '../../assets/relogio-calendario.png'
+import imgMoney from '../../assets/dinheiro.png'
+import imgForum from '../../assets/forum.png'
+import imgSettings from '../../assets/configuracoes.png'
+import { NavLink } from 'react-router-dom'
+const SidebarTenant = () => {
+  return (
+    <>
+        <nav className='calendarMain'>
+            <NavLink to="/*">
+                <img className='img' src={imgCalendar} />
+            </NavLink>
+            <NavLink to="/*">
+                <img className='img' src={imgSuitcase} />
+            </NavLink>
+            <NavLink to="/*">
+                <img className='img' src={imgCalendarClock} />
+            </NavLink>
+            <NavLink to="/*">
+                <img className='img' src={imgMoney} />
+            </NavLink>
+            <NavLink to="/*">
+                <img className='img' src={imgForum} />
+            </NavLink>
+            <NavLink to="/configurationAdm">
+                <img className='img' src={imgSettings} />
+            </NavLink>
+        </nav>
+    </>
+  )
+}
+
+export default SidebarTenant;

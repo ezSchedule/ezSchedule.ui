25b26f3cd4e606195e309e26f3450d355bde3869
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-11 15:00:31 -0300

    inicio pagina de serviços do tenant

---
+++ front-ezchedule/src/pages/Tenant/ServicesTenant/Index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './servicesTenant.css'
+import SidebarTenant from '../../../components/internal/SidebarTenant'
+const ServicesTenant = () => {
+  return (
+    <>
+        <div className='mainServicesTenant'>
+            <SidebarTenant />
+        </div>
+    </>
+  )
+}
+
+export default ServicesTenant

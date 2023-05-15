6fbc82d61886d15a00c44c619ef5712c47cf4536
VinyProgramador <vinicius.nunes@sptech.school>
2023-05-15 18:43:56 -0300

    feat

---
+++ front-ezchedule/src/pages/Tenant/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './paymentTenant.css'
+import SidebarTenant from '../../components/internal/SidebarTenant/Index'
+import HeaderInternal from '../../components/internal/Header'
+import ScheduleBox from '../../components/internal/ScheduledBox'
+import ScheduleBoxAdd from '../../components/internal/ScheduleBoxAdd'
+const PaymentTenant = () => {
+    return (
+        <>
+            <div className='mainPaymentTenant'>
+                <SidebarTenant />
+                <HeaderInternal text="Datas Agendadas" />
+            </div>
+            <div className='paymentTenant'>
+                <div className='paymentTenantScroll'>
+                    <div className='paymentTenantScrollBoxes'>
+                        <ScheduleBox />
+                        <ScheduleBoxAdd />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default PaymentTenant

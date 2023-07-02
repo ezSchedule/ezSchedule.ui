798357cc63cf8dc58df3e61cf9607beb2f500e76
VinyProgramador <vinicius.nunes@sptech.school>
2023-07-02 19:44:57 -0300

    :sparkles: busca de tenant na hora de adicionar um novo servico funcional.

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,24 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./modal.css";
 import Close from "../assets/fechar.png";
+import ImageUserDefault from "../assets/Perfil.png"
+import SectionTenantData from "../../components/internal/SectionTenantData";
 
-const Modal = ({ isOpen, children, setModalOpen, title }) => {
+const Modal = ({ isOpen, children, setModalOpen, title, tenantList }) => {
+    const [searchValue, setSearchValue] = useState("");
+    const [filteredTenants, setFilteredTenants] = useState([]);
+
+    useEffect(() => {
+        setFilteredTenants(tenantList);
+    }, [tenantList]);
+
+    const handleSearch = (e) => {
+        const searchInput = e.target.value.toLowerCase();
+        const filtered = tenantList.filter(
+            (tenant) => tenant.name.toLowerCase().includes(searchInput)
+        );
+        setFilteredTenants(filtered);
+        setSearchValue(searchInput);
+    };
+
     if (isOpen) {
         return (
             <div className="bg-modal">
                 <div className="card-modal">
                     <div className="header-modal">
                         <h2>{title}</h2>
-                        <img onClick={setModalOpen} src={Close} />
+                        <input
+                            className="input-search"
+                            type="text"
+                            placeholder="Buscar..."
+                            value={searchValue}
+                            onChange={handleSearch}
+                        />
+                        <img onClick={setModalOpen} src={Close} alt="Close" />
                     </div>
                     <div className="body-modal">
-                        {children}
+                        {filteredTenants.length === 0 ? (
+                            <div className="div-not-content">
+                                <p>Nenhum condomínio encontrado.</p>
+                            </div>
+                        ) : (
+                            <div className="container-list-tenant">
+                                {filteredTenants.map((tenant) => (
+                                    <SectionTenantData
+                                        key={tenant.id}
+                                        img={
+                                            tenant.nameBlobImage == null
+                                                ? ImageUserDefault
+                                                : "https://ezscheduleusersimages.blob.core.windows.net/ezschedules/" +
+                                                tenant.nameBlobImage
+                                        }
+                                        id={tenant.id}
+                                        name={tenant.name}
+                                        apartment={tenant.apartmentNumber}
+                                        block={tenant.residentsBlock}
+                                        insertId={() => { }}
+                                    />
+                                ))}
+                            </div>
+                        )}
                     </div>
                 </div>
             </div>
         );
     }
     return null;
-}
+};
 
 export default Modal;
--- src/hooks/urlBase.js
+++ src/hooks/urlBase.js
@@ -1,3 +1,3 @@
-const urlBase = "http://ec2-3-90-199-139.compute-1.amazonaws.com:8080/";
+const urlBase = "http://localhost:8080/";
 
 export default urlBase;
